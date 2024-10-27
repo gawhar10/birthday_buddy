17ea1fb0dd5288147418542dbaa2d0d2ff1c816f
gawhar <gawharsalman@outlook.com>
2024-10-27 22:59:38 +0530

    deleted:    public/favicon.ico 	modified:   public/index.html 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	deleted:    public/manifest.json 	deleted:    public/robots.txt 	deleted:    src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/birthday.js 	modified:   src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { birtdayList } from "./birthday";
+import React from "react";
 
-function App() {
+const App = () => {
+  const [list, setList] = React.useState(birtdayList);
+  const removeBtnHandler = (id) => {
+    let listAfterRemove = list.filter((item) => item.id !== id);
+    setList(listAfterRemove);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <h1>Birthday Buddy App</h1>
+      {list.map((item, index) => {
+        return (
+          <article key={index}>
+            <Friend item={item} />
+            <button
+              className="remove_btn"
+              onClick={() => removeBtnHandler(item.id)}
+            >
+              Remove
+            </button>
+          </article>
+        );
+      })}
+      <button className="clear_all_btn" onClick={() => setList([])}>
+        Remove All
+      </button>
+    </section>
   );
-}
+};
+
+const Friend = (props) => {
+  const { img, name, age } = props.item;
+
+  return (
+    <>
+      <div>
+        <img src={img} alt="birthday"></img>
+        <div>
+          <h1>{name}</h1>
+          <p>{age} Years</p>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default App;
---
+++ src/birthday.js
@@ -0,0 +1,32 @@
+export const birtdayList = [
+  {
+    id: 0,
+    img: "https://images.unsplash.com/photo-1615304048636-47fa618f3744?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Dennis Fraevich",
+    age: "32",
+  },
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1611601322175-ef8ec8c85f01?q=80&w=1064&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "Andrea Vettori",
+    age: "34",
+  },
+  {
+    id: 2,
+    img: "https://images.unsplash.com/photo-1585984622591-5af87d8b41ed?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fG1vZGVscyUyMHBob3Rvc3xlbnwwfHwwfHx8MA%3D%3D",
+    name: "Bill Odle",
+    age: "40",
+  },
+  {
+    id: 3,
+    img: "https://images.unsplash.com/photo-1590769620285-6926a01c2a58?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fHw%3D",
+    name: "Arthur Loukas",
+    age: "45",
+  },
+  {
+    id: 4,
+    img: "https://images.unsplash.com/photo-1621245550592-63b5ed0ef1da?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    name: "El gringo",
+    age: "37",
+  },
+];
